b15d99f3cb9818f539471dc12e10585875234cbf
CNavo <chrisnavo01@gmail.com>
2021-05-31 22:43:08 -0700

    Added Location params

--- app/src/androidTest/java/com/example/helloworld/SignInActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/SignInActivityTest.java
@@ -103,13 +103,10 @@ public class SignInActivityTest {
 
     @Test
     public void testMatches() {
-        onView(isRoot()).perform(waitFor(1000));
         // Open Drawer to click on navigation.
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
-        onView(isRoot()).perform(waitFor(1000));
         onView(withText(R.string.matches))
                 .perform(click()); // Select nav button in nav drawer
-        onView(isRoot()).perform(waitFor(1000));
         //onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.scrollToPosition(1));
         //onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(1, new TestUtils.ClickOnLikeButton()));
         //onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(1, new TestUtils.ClickOnLikeButton()));
